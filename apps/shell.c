a3b13e888d5d596e1d873ba2141034e3957e0cba
Armemius <rusgy@yandex.ru>
2025-01-17 18:58:26 +0300

    [+] Init CMake C project

---
+++ apps/shell.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <shell/process.h>
+
+int main(void) {
+  say_hello();
+  return 0;
+}
---
+++ src/shell/process.c
@@ -0,0 +1,5 @@
+#include <shell/process.h>
+
+#include <stdio.h>
+
+void say_hello(void) { printf("Hello, World!\n"); }
