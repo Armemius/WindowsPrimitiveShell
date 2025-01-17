18c1eca4d6facccf19713bdd9efc090c65fa9a19
Armemius <rusgy@yandex.ru>
2025-01-17 22:00:40 +0300

    [+] Update executable and set version

--- apps/shell.c
+++ apps/shell.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
 #include <shell/process.h>
 
 int main(void) {
-  say_hello();
-  return 0;
+  RunShell();
+
+  return EXIT_SUCCESS;
 }
